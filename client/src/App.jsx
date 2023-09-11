efa88daa1175fbab96aa03c748192ff3e585c365
wihctoh <rivertoxi911@gmail.com>
2023-09-11 11:43:18 +0300

    fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,9 @@
-import { Routes, Route } from "react-router-dom";
 import TitlePage from "./pages/TitlePage/TitlePage";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<TitlePage />}></Route>
-      </Routes>
+      <TitlePage />
     </>
   );
 }
